6c0148777fb9c66ee403025f036199fc66cf0f36
himreaowi <himariang@gmail.com>
2023-08-16 17:59:01 +0800

    Update files

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+        loader: 'akamai',
+        path: '',
+    },
+    assetPrefix: './',
     output: 'export',
-    basePath: ''
 }
 
 module.exports = nextConfig
