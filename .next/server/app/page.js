ee820c91e30453c65aae9822da2ae94530d1e177
himreaowi <himariang@gmail.com>
2023-08-16 10:46:54 +0800

    Please work

--- .next/server/app/page.js
+++ .next/server/app/page.js
@@ -244,7 +244,7 @@ module.exports = require("url");
 
 /***/ }),
 
-/***/ 825:
+/***/ 7348:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 "use strict";
@@ -1029,7 +1029,7 @@ __webpack_require__.r(__webpack_exports__);
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, [587,590], () => (__webpack_exec__(825)));
+var __webpack_exports__ = __webpack_require__.X(0, [587,590], () => (__webpack_exec__(7348)));
 module.exports = __webpack_exports__;
 
 })();
